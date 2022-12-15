23b4399b169b42c9a3367205fce5d7c37e4a8e98
Alexandra Miranda <alexyamiranda7@gmail.com>
2022-12-14 22:21:26 -0300

    feat/ estilo da LoginPage para desktop

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,10 +1,68 @@
 import React from "react";
 import { StyledLoginPage } from "./styles";
+import logo from "../../img/kenzieBurguer.svg";
+import bag from "../../img/shoppingBag.svg";
+import circulos from "../../img/circulos.svg";
+import { useNavigate } from "react-router-dom";
+
+const Logo = logo;
+const Bag = bag;
+const Circulos = circulos;
 
 export function LoginPage() {
+  const navigate = useNavigate();
+  function goLoginClick() {
+    navigate("/register");
+  }
   return (
     <StyledLoginPage>
-      <h1>Estou no Login</h1>
+      <section className="secLogin">
+        <h1 className="titleLogin">Login</h1>
+        <form className="formLogin">
+          <label htmlFor="name" className="lbLogin">
+            Nome
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            className="iptLogin"
+          />
+          <label htmlFor="name" className="lbLogin">
+            Senha
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Digite sua senha"
+            className="iptLogin"
+          />
+          <button type="submit" className="btLogar">
+            Logar
+          </button>
+        </form>
+        <p className="messageCreateRegister">
+          Crie sua conta para saborear muitas delícias e matar sua fome!
+        </p>
+        <button className="btGoToRegister" onClick={goLoginClick}>
+          Cadastrar
+        </button>
+      </section>
+      <section className="secApresentation">
+        <img src={logo} alt="imagem de Kenzie Burguer" />
+        <div className="areaImageAndMessage">
+          <figure className="areaBag">
+            <img src={bag} alt="imagem de sacola verde" />
+          </figure>
+          <p className="reminder">
+            A vida é como um sanduíche, é preciso recheá-la com os{" "}
+            <strong className="bests">melhores</strong> ingredientes.
+          </p>
+        </div>
+        <figure>
+          <img src={circulos} alt="imagem de circulos de cor cinza" />
+        </figure>
+      </section>
     </StyledLoginPage>
   );
 }
--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -1,3 +1,181 @@
 import styled from "styled-components";
 
-export const StyledLoginPage = styled.div``;
+export const StyledLoginPage = styled.div`
+  max-width: 100vw;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 3.875rem;
+
+  .secLogin {
+    max-width: 31.25rem;
+    width: 100%;
+    height: 34.8125rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-radius: 0.25rem;
+    border: 0.1125rem solid #f5f5f5;
+    padding: 1.625rem 1.5rem;
+    background-color: #ffffff;
+  }
+
+  .titleLogin {
+    margin-bottom: 0.875rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 700;
+    font-size: 1.125rem;
+    color: #333333;
+  }
+
+  .formLogin {
+    max-width: 28.25rem;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .lbLogin {
+    margin-bottom: 0.3125rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    font-size: 0.75rem;
+    color: #999999;
+  }
+
+  .iptLogin {
+    max-width: 28.25rem;
+    width: 100%;
+    height: 4.375rem;
+    margin-bottom: 0.9375rem;
+    padding: 1.25rem 0.9375rem;
+    margin-bottom: 0.3125rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    color: #333333;
+    outline: none;
+    border: 0.1125rem solid #e0e0e0;
+    border-radius: 0.5rem;
+  }
+
+  .iptLogin::placeholder {
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+
+  .iptLogin:focus {
+    border: 0.1125rem solid #333333;
+    border-radius: 0.5rem;
+  }
+
+  .btLogar {
+    max-width: 28.25rem;
+    width: 100%;
+    height: 3.75rem;
+    margin-top: 1.1875rem;
+    margin-bottom: 1.1875rem;
+    background-color: #219653;
+    border-radius: 0.5rem;
+    padding: 0rem 2.5rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 600;
+    font-size: 1rem;
+    color: #ffffff;
+  }
+  .btLogar:hover {
+    background-color: #93d7af;
+  }
+
+  .messageCreateRegister {
+    margin-left: 5.375rem;
+    margin-bottom: 1.3125rem;
+    max-width: 20.4375rem;
+    width: 100%;
+    height: 2.375rem;
+    text-align: center;
+    line-height: 1.125rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 400;
+    font-size: 0.875rem;
+    color: #999999;
+  }
+
+  .btGoToRegister {
+    max-width: 28.25rem;
+    width: 100%;
+    height: 3.75rem;
+    margin-top: 1.1875rem;
+    margin-bottom: 1.1875rem;
+    background-color: #f5f5f5;
+    border-radius: 0.5rem;
+    padding: 0rem 2.5rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 600;
+    font-size: 1rem;
+    color: #999999;
+  }
+
+  .btGoToRegister:hover {
+    background-color: #828282;
+    color: #e0e0e0;
+  }
+
+  .secApresentation {
+    max-width: 31.25rem;
+    width: 100%;
+    height: 34.8125rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 1.625rem 1.5rem;
+    background-color: #ffffff;
+  }
+
+  .areaImageAndMessage {
+    margin-top: 1.875rem;
+    margin-bottom: 1.875rem;
+    max-width: 23.5625rem;
+    width: 100%;
+    height: 5.9375rem;
+    display: flex;
+    padding: 1.0625rem 0rem 1.125rem 0.875rem;
+    gap: 1.1875rem;
+    background-color: #ffffff;
+    border: 0.0625rem solid #e0e0e0;
+    border-radius: 0.3125rem;
+  }
+
+  .areaBag {
+    max-width: 3.75rem;
+    width: 100%;
+    height: 3.75rem;
+    background: rgba(39, 174, 96, 0.1);
+    border-radius: 0.3125rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .reminder {
+    max-width: 16.3125rem;
+    width: 100%;
+    height: 4.125rem;
+    font-family: "Inter", sans-serif;
+    font-weight: 500;
+    font-size: 0.875rem;
+    line-height: 1.375rem;
+    color: #828282;
+  }
+
+  .bests {
+    font-family: "Inter", sans-serif;
+    font-weight: 500;
+    font-size: 0.875rem;
+    line-height: 1.375rem;
+    color: #333333;
+  }
+`;
